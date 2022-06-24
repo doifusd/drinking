9e135ee97d116f9be8320f75d39f57fc1f5753d5
zzj <zhangzijian@miage.com.cn>
2022-06-24 13:44:13 +0800

    feat:1.0

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"drinkingtime/pkg/img"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"github.com/robfig/cron/v3"
+)
+
+var myApp fyne.App
+
+func main() {
+	myApp = app.New()
+	myApp.SetIcon(img.ResourceIconPng)
+	c := cron.New(cron.WithSeconds())
+	c.AddFunc("01 30 7,9,11,13,15,17,19,21 * * *", func() {
+		// c.AddFunc("*/5 * * * * *",func(){
+		drinkImage()
+	})
+	c.Start()
+	myApp.Run()
+	select {}
+}
+
+func drinkImage() {
+	w := myApp.NewWindow("Drinking time")
+	w.CenterOnScreen()
+	w.SetTitle("Drinking time")
+	image := canvas.NewImageFromResource(img.ResourceDrinkJpeg)
+	w.SetContent(image)
+	w.Resize(fyne.NewSize(1920, 1080))
+	w.CenterOnScreen()
+	w.Show()
+	w.SetCloseIntercept(func() {
+		w.Hide()
+	})
+	st := time.Now()
+	go func() {
+		for range time.Tick(time.Second) {
+			if time.Now().Sub(st) >= 3*time.Second {
+				w.Hide()
+				break
+			}
+		}
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module drinkingtime
+
+go 1.16
+
+require (
+	fyne.io/fyne/v2 v2.2.1
+	github.com/robfig/cron/v3 v3.0.1
+)
